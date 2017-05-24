6a04e3c0b91e4cc12d8119d318d2de09d28b4d44
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-05-24 19:58:15 +0530

    Changes for checking cookie and adding client object ref

--- router/project/router.js
+++ router/project/router.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 var cookieParser = require('cookie-parser');
 
 ProjectRouter.use(cookieParser());
-ProjectRouter.use(session({ secret: 'secretkey', cookie: { httpOnly: false,secure:false,expires: new Date(Date.now() + (300 * 1000))} })); // session secret
+ProjectRouter.use(session({ secret: 'secretkey', cookie: { httpOnly: false,secure:false,expires: new Date(Date.now() + (1*24*60*60*1000))} })); // session secret
 
 ProjectRouter.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -18,13 +18,13 @@ ProjectRouter.use(function(req, res, next){
     } else {
         res.cookie("token", "", { expires: new Date() });
         res.json({data: {status : 401}});
-    }
+    }/*next()*/;
 });
 
 ProjectRouter.get('/getAllProjects', function(req, res, next){
 	
 	var userId = req.cookies['token'].split('-')[1];
-	ProjectModel.find({'userId': userId}).exec(function(err, projects){
+	ProjectModel.find({'userId': userId}).populate('client').exec(function(err, projects){
 		if(err) res.json({data:{status : 500}});
 		res.json({data: {status : 200, projects}});
 	})
@@ -34,11 +34,11 @@ ProjectRouter.get('/getAllProjects', function(req, res, next){
 ProjectRouter.post('/addProject', function(req, res, next){
 	
 	var userId = req.cookies['token'].split('-')[1];
+	//var userId = "591ac03b5f2cf028a0124b6b";
 	
 	var newProject = new ProjectModel();
 	newProject.projectName = req.body.projectName;
-	newProject.clientId = req.body.clientId;
-	newProject.clientName = req.body.clientName;
+	newProject.client = req.body.client;
 	newProject.completionFlag = req.body.completionFlag;
 	newProject.userId = userId;
 	newProject.description = req.body.description;
