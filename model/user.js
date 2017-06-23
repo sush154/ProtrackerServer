7c251b4c027243644c527a097366b2fc19038d67
SushantJoshi-Cgnizant <Sushant.Joshi@cognizant.com>
2017-06-23 19:22:45 +0530

    Mongo model issue fix

--- model/user.js
+++ model/user.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 var userSchema = require('../schema/user');
 
-var UserModule = mongoose.model('UserModel', userSchema, 'user');
+var UserModule = mongoose.model('user', userSchema);
 
 module.exports = UserModule;
