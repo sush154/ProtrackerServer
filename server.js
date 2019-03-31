ff0549ee0c4f1487865a03c999b2e0b095a70c7a
sush154 <sushant.pj41288@gmail.com>
2019-03-31 17:13:09 +0530

    adding process port number

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var app = require('./app');
-app.listen(9000, function(){
+const PORT = process.env.PROCESS || 9000;
+app.listen(PORT, function(){
 	console.log("app is listening on port 9000");
 })
